92c8e4586af52983f5a28ffa6098a575d2461b6c
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-05 11:16:41 -0400

    created ASP.NET MVC project

---
+++ CollaborationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CollaborationDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
