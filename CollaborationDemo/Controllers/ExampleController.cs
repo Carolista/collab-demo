bfec6bbb9e6d43815464b22f37ae7d5c4165f631
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-05 11:44:01 -0400

    coded solution for example

---
+++ CollaborationDemo/Controllers/ExampleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollaborationDemo;
+
+[Route("/example")]
+public class ExampleController : Controller
+{
+    [HttpGet("")]
+    public IActionResult RenderExamplePage()
+    {
+        return View("Index");
+    }
+
+    [HttpGet("form")]
+    public IActionResult RenderExampleForm()
+    {
+        return View("Form");
+    }
+
+    [HttpPost("form")]
+    public IActionResult ProcessExampleForm(string name, string email, string comment)
+    {
+        ViewBag.Name = name;
+        return View("ThankYou");
+    }
+
+    // [HttpGet("thankyou")]
+}
